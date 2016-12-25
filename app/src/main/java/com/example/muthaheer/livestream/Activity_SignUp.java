bdcd3b1d8f99cff9c4dce0f70afda639a5366c5f
Muthaheer <muthaheer1207@gmail.com>
2016-12-25 18:11:06 +0530

    Added Activity_SignUp.xml

---
+++ app/src/main/java/com/example/muthaheer/livestream/Activity_SignUp.java
@@ -0,0 +1,13 @@
+package com.example.muthaheer.livestream;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__sign_up);
+    }
+}
