cf351842cb2d4ab795fe50c22399c9baaebc9278
Muthaheer <muthaheer1207@gmail.com>
2016-12-30 20:47:17 +0530

    update: Activity_SignUp.java

--- app/src/main/java/com/example/muthaheer/livestream/Activity_SignUp.java
+++ app/src/main/java/com/example/muthaheer/livestream/Activity_SignUp.java
@@ -5,26 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Activity_SignUp extends AppCompatActivity {
 
-    TextView _sigin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__sign_up);
 
-        _sigin=(TextView) findViewById(R.id.link_login);
-
-
-        _sigin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Activity_SignUp.this, Activity_signin.class);
-                startActivity(i);
-            }
-        });
     }
-
+ public void clk(View v)
+ {
+     Intent i=new Intent(this,Activity_signin.class);
+     startActivity(i);
+ }
 }
