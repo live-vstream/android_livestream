4db28a4b2e563f8a3d9428c0eba08fc8ed5dc862
Kashif Minhaj <dailypctips@gmail.com>
2017-01-12 20:29:42 +0530

    Rename activites to follow standard conventions

---
+++ app/src/main/java/com/example/muthaheer/livestream/SigninActivity.java
@@ -0,0 +1,190 @@
+package com.example.muthaheer.livestream;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.muthaheer.livestream.app.AppConfig;
+import com.example.muthaheer.livestream.app.AppController;
+import com.example.muthaheer.livestream.helper.SessionManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SigninActivity extends AppCompatActivity {
+    private static final String TAG = "Activity_signin";
+
+    EditText _nameText;
+    EditText _passwordText ;
+    Button _loginButton ;
+
+    private SessionManager session;
+    ProgressDialog progressDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        _nameText = (EditText)findViewById(R.id.input_name);
+        _passwordText = (EditText) findViewById(R.id.input_password);
+        _loginButton = (Button) findViewById(R.id.btn_signin);
+
+        // Session manager
+        session = new SessionManager(getApplicationContext());
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        progressDialog = new ProgressDialog(SigninActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        final String name = _nameText.getText().toString();
+        final String password = _passwordText.getText().toString();
+
+
+
+                        checkLogin(name, password);
+
+    }
+
+
+    public void onLoginSuccess() {
+        _loginButton.setEnabled(true);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        _loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = _nameText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (name.isEmpty()) {
+            _nameText.setError("Enter a valid Name");
+            valid = false;
+        } else {
+            _nameText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4) {
+            _passwordText.setError("Password length must be > 4");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
+    }
+
+    /**
+     * function to verify login details
+     * */
+    private void checkLogin(final String name, final String password) {
+        // Tag used to cancel the request
+        String tag_string_req = "req_login";
+
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                AppConfig.URL_LOGIN, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Login Response: " + response.toString());
+
+                progressDialog.dismiss();
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean success = jObj.getBoolean("success");
+
+                    // Check for error node in json
+                    if (success) {
+                            session.setLogin(true);
+
+                            // Launch main drawer activity
+                           Intent intent = new Intent(SigninActivity.this,
+                                    MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    else {
+                        // Error in login. Get the error message
+                        String errorMsg = jObj.getString("msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Json error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", name);
+                params.put("password", password);
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+}
