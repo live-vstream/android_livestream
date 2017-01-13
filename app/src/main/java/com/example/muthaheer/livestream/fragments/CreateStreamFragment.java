11ac4824ee6a4a90bdd798dc300913c2c9c91536
Muthaheer <muthaheer1207@gmail.com>
2017-01-13 19:21:20 +0530

    Add missing files to git

---
+++ app/src/main/java/com/example/muthaheer/livestream/fragments/CreateStreamFragment.java
@@ -0,0 +1,101 @@
+package com.example.muthaheer.livestream.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.muthaheer.livestream.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link CreateStreamFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link CreateStreamFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CreateStreamFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+    private Button mCreateButton;
+    private EditText mStreamNameET;
+
+    public CreateStreamFragment() {
+        // Required empty public constructor
+    }
+
+    public static CreateStreamFragment newInstance() {
+        CreateStreamFragment fragment = new CreateStreamFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+
+        mCreateButton = (Button) getActivity().findViewById(R.id.create_stream_btn);
+        mStreamNameET = (EditText) getActivity().findViewById(R.id.create_stream_name);
+
+        mCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mListener != null) {
+                    mListener.onFragmentInteraction(mStreamNameET.getText().toString());
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_stream, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(String streamName);
+    }
+}
